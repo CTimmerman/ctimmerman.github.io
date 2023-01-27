ddf6e44bf9ffa8f1449eaf506ceae615451cd074
Cees Timmerman <CTimmerman@users.noreply.github.com>
2023-01-27 15:01:03 +0100

    PWA

---
+++ chess/serviceWorker.js
@@ -0,0 +1,22 @@
+const assets = [
+	"/",
+	"/index.html",
+	"/rook.png",
+	"/think.png",
+]
+
+self.addEventListener("fetch", fetchEvent => {
+	fetchEvent.respondWith(
+		caches.match(fetchEvent.request).then(res => {
+			return res || fetch(fetchEvent.request)
+		})
+	)
+})
+
+self.addEventListener("install", installEvent => {
+	installEvent.waitUntil(
+		caches.open("chess").then(cache => {
+			cache.addAll(assets)
+		})
+	)
+})
