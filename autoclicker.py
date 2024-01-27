11fd45d214db411ae74c595fa7a8ec49a6844d39
Cees Timmerman <CTimmerman@users.noreply.github.com>
2024-01-27 14:00:08 +0100

    Try to avoid clicking randum stuff when moving the cursor.

--- autoclicker.py
+++ autoclicker.py
@@ -5,19 +5,26 @@ import keyboard, mouse  # pip install keyboard mouse
 
 click = False
 hotkey = "Alt + C"
-
-print(__doc__, hotkey, "to toggle. Ctrl + C to exit.")
+target = (0, 0)
 
 def toggle_clicker():
-  global click
+  global click, target
   click = not click
-  print("Clicking", click)
+  if click:
+    target = mouse.get_position()
+  print("Clicking", target, click)
 
 keyboard.add_hotkey(hotkey, toggle_clicker)
+print(__doc__, hotkey, "to toggle. Ctrl + C to exit.")
 
 try:
   while True:
-    if click: mouse.click()
+    p = mouse.get_position()
+    if click and (abs(target[0] - p[0]) > 40 or abs(target[1] - p[1]) > 40):
+      toggle_clicker()
+    if click:
+      mouse.move(*target, True)
+      mouse.click()
     time.sleep(0.001)
 except KeyboardInterrupt:
   pass
