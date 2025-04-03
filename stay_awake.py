0f131cd59e99e81adbe173db56589d736f9983c2
Cees Timmerman <CTimmerman@users.noreply.github.com>
2025-04-03 09:19:38 +0200

    Thought this was in already.

---
+++ stay_awake.py
@@ -0,0 +1,13 @@
+"""Stop messing up my LLM interence by nodding off, Windows 11. CT 2024-12-19."""
+
+import time
+
+from wakepy import keep
+
+print("Staying up.")
+
+with keep.running():  # presenting() to also keep on display.
+    while True:
+        time.sleep(60)
+
+print("Done.")
