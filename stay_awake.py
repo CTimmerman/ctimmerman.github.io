415e954c9871c90aac3a525c4d7a2d97e5c7e615
Cees Timmerman <CTimmerman@users.noreply.github.com>
2025-06-20 17:27:21 +0200

    Detect lock on GNU/Linux using systemd.

--- stay_awake.py
+++ stay_awake.py
@@ -1,4 +1,4 @@
-"""Stop messing up my LLM inference by nodding off, Windows 11. CT 2024-12-19 to 2025-05-27."""
+"""Stop messing up my LLM inference by nodding off, Windows 11. CT 2024-12-19 to 2025-06-20."""
 
 import platform, subprocess, time
 
@@ -10,34 +10,36 @@ from wakepy import keep
 
 
 def screen_locked() -> bool:
-	match platform.system():
-		case "Darwin":
-			import Quartz
-			d = Quartz.CGSessionCopyCurrentDictionary()
-			return bool(d.get("CGSSessionScreenIsLocked", 0))
-		case "Linux":
-			cmd = '''gnome-screensaver-command -q | grep "is active"'''
-			return bool(subprocess.check_output(cmd))
-		case "Windows":
-			cmd = "tasklist"
-			return "LogonUI" in str(subprocess.check_output(cmd))
-	return False
+    match platform.system():
+        case "Darwin":
+            import Quartz  # type: ignore  # pylint: disable=import-outside-toplevel
+
+            d = Quartz.CGSessionCopyCurrentDictionary()
+            return bool(d.get("CGSSessionScreenIsLocked", 0))
+        case "Linux":
+            # Assume systemd.
+            return b"yes" in subprocess.check_output(
+                ["loginctl", "show-session", "-p", "IdleHint"]
+            )
+        case "Windows":
+            return b"LogonUI" in subprocess.check_output("tasklist")
+    return False
 
 
 if __name__ == "__main__":
-	print("Staying up.")
-	fun = keep.presenting
-	while True:
-		with fun():
-			while True:
-				time.sleep(20)
-				old_fun = fun
-				if screen_locked():
-					fun = keep.running
-				else:
-					fun = keep.presenting
-				if old_fun != fun:
-					print(f"Screen {'un' if fun.__name__ == 'presenting' else ''}locked on {time.ctime()}. Keep {fun.__name__}")
-					break
-
-	print("Done.")
+    print("Staying up.")
+    fun = keep.presenting
+    while True:
+        with fun():
+            while True:
+                time.sleep(20)
+                old_fun = fun
+                if screen_locked():
+                    fun = keep.running
+                else:
+                    fun = keep.presenting
+                if old_fun != fun:
+                    print(
+                        f"Screen {'un' if fun.__name__ == 'presenting' else ''}locked on {time.ctime()}. Keep {fun.__name__}"
+                    )
+                    break
