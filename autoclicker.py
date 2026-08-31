d52fd41234b057762d4278f2ceec692f74c835ea
Cees Timmerman <CTimmerman@users.noreply.github.com>
2026-08-31 20:59:31 +0200

    Fix Gundam favicon and autoclicker breaking on Windows lock

--- autoclicker.py
+++ autoclicker.py
@@ -1,13 +1,12 @@
-"""Autoclicker by Cees Timmerman, 20230613 - 20260801.
-
-When Windows forgets, hit Ctrl+C to reregister the hotkey.
+"""Autoclicker by Cees Timmerman, 20230613 - 20260831.
 """
 
+import platform
 import time
-import keyboard, mouse  # pip install keyboard mouse
+import mouse  # pip install mouse
 
-click = False
 HOTKEY = "Alt + C"
+click = False
 target = (0, 0)
 
 def toggle_clicker():
@@ -17,22 +16,26 @@ def toggle_clicker():
     target = mouse.get_position()
   print("Clicking", target, click)
 
-while True:
-  try:
-    keyboard.remove_all_hotkeys()
-  except AttributeError:
-    pass
-  keyboard.add_hotkey(HOTKEY, toggle_clicker)
-  print(__doc__, HOTKEY, "to toggle. Ctrl + C to exit.")
+print(__doc__, HOTKEY, "to toggle. Ctrl + C to exit.")
 
-  try:
-    while True:
-      p = mouse.get_position()
-      if click and (abs(target[0] - p[0]) > 40 or abs(target[1] - p[1]) > 40):
-        toggle_clicker()
-      if click:
-        mouse.move(*target, True)
-        mouse.click()
-      time.sleep(0.001)  # 0.0001 might be related to Windows 11 GameInput BSOD.
-  except KeyboardInterrupt:
-    pass
+
+if platform.system() == "Windows":
+  from global_hotkeys import *  # pip install global-hotkeys
+  bindings = [[HOTKEY, toggle_clicker, None],]
+  register_hotkeys(bindings)
+  start_checking_hotkeys()
+else:
+  import keyboard
+  keyboard.add_hotkey(HOTKEY, toggle_clicker)
+
+try:
+  while True:
+    p = mouse.get_position()
+    if click and (abs(target[0] - p[0]) > 40 or abs(target[1] - p[1]) > 40):
+      toggle_clicker()
+    if click:
+      mouse.move(*target, True)
+      mouse.click()
+    time.sleep(0.01)  # 0.0001 might be related to Windows 11 GameInput BSOD.
+except KeyboardInterrupt:
+  pass
